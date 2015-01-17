71c2394524c010b64e4add3ff4f41fcc217988b7
ssyi <ssyi@MorganSungsu-Yiui-MacBook-Pro.local>
2015-01-17 21:48:35 +0900

    Add sprintf test

---
+++ stdio/test/test_runners/AllTests.c
@@ -0,0 +1,13 @@
+#include "unity.h"
+#include "unity_fixture.h"
+
+static void RunAllTests(void)
+{
+	RUN_TEST_GROUP(sprintf);
+}
+
+int main(int argc, char * argv[])
+{
+	return UnityMain(argc, argv, RunAllTests);
+}
+
