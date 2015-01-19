2eda8579007ed0281c22ac7ba06a13c59e8f0957
ssyi <ssyi@MorganSungsu-Yiui-MacBook-Pro.local>
2015-01-20 06:58:53 +0900

    1. Add TurnOff function, 2. Refactoring test file

--- LedDriver/src/LedDriver.h
+++ LedDriver/src/LedDriver.h
@@ -2,4 +2,5 @@
 
 void LedDriver_Create(uint16_t * address);
 void LedDriver_TurnOn(int ledNumber);
+void LedDriver_TurnOff(int ledNumber);
 
--- LedDriver/test/test_runners/LedDriverTestRunner.c
+++ LedDriver/test/test_runners/LedDriverTestRunner.c
@@ -5,5 +5,6 @@ TEST_GROUP_RUNNER(LedDriver)
 {
 	RUN_TEST_CASE(LedDriver, LedsOffAfterCreate);
 	RUN_TEST_CASE(LedDriver, TurnOnLedOne);
+	RUN_TEST_CASE(LedDriver, TurnOffLedOne);
 }
 
